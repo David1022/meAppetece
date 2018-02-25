fb8590a23168e2a706f9cfec2d177496c513a7a6
david <dav_mm@hotmail.com>
2018-02-26 00:51:43 +0100

    splash mas fragment presentacion

---
+++ app/src/main/java/com/appdesigndm/meappetece/PresentationFragment.java
@@ -0,0 +1,26 @@
+package com.appdesigndm.meappetece;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class PresentationFragment extends Fragment {
+
+
+    public PresentationFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_presentation, container, false);
+        return view;
+    }
+
+}
--- app/src/main/java/com/appdesigndm/meappetece/QuestionActivity.java
+++ app/src/main/java/com/appdesigndm/meappetece/QuestionActivity.java
@@ -9,5 +9,7 @@ public class QuestionActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_question);
+
+        getSupportFragmentManager().beginTransaction().add(R.id.fragment_container, new PresentationFragment()).commit();
     }
 }
